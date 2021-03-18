f674c56f675724ab62084dfe382caea09c39aab4
Andrei MATEI (78626) <andrei.matei1704@stud.acs.upb.ro>
2021-03-18 22:37:39 +0200

    empty project

---
+++ src/main/java/com/task/ING/IngTaskApplication.java
@@ -0,0 +1,13 @@
+package com.task.ING;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngTaskApplication.class, args);
+    }
+
+}
